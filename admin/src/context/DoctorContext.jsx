e23ab526f23560accc3d7a7ca2e0a541a056c810
Fahad <fahadhashmi182@gmail.com>
2026-06-27 00:07:06 +0530

    Complete MediGo

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -37,7 +37,7 @@ const DoctorContextProvider = (props) => {
         try {
 
             const { data } = await axios.get(backendUrl + '/api/doctor/profile', { headers: { dToken } })
-            console.log(data.profileData)
+            // console.log(data.profileData)
             setProfileData(data.profileData)
 
         } catch (error) {
